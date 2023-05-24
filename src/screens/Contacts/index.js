a4202f01152fc748bcc66ef20e89b8acaf804529
kishananish <kishan.nodejs@gmail.com>
2023-05-24 20:12:50 +0530

    Input buttons and stylesheet

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Container from '../../components/common/Container';
 const Contacts = () => {
   return (
-    <View>
+    <Container style={{color: 'red', padding: 100}}>
       <Text>Hi from contacts</Text>
-    </View>
+    </Container>
   );
 };
 
